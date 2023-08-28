e586be430d60a41e8ecb91cbd243ee187cf8cc02
ryoma <ryomams@users.noreply.github.com>
2023-08-27 18:53:19 -0700

    failed attempt; does not render

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // https://threejs.org/manual/#en/fog
 
 import * as THREE from 'three';
-//import {OrbitControls} from 'three/addons/controls/OrbitControls.js'; TODO
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
 
 //declarations for ease of use
@@ -13,19 +13,6 @@ const Y_AXIS = new THREE.Vector3(0, 1, 0);
 const TARGET_HEIGHT = 720
 const TARGET_WIDTH = 1280
 
-// renderer and camera
-const camera = new THREE.PerspectiveCamera(90, TARGET_WIDTH / TARGET_HEIGHT, 0.1, 1000);
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
-
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(TARGET_WIDTH, TARGET_HEIGHT);
-renderer.setClearColor(0x280A8C, 1);
-document.body.appendChild(renderer.domElement);
-
-const box_geometry = new THREE.BoxGeometry(1, 1, 1);
-const box_material = new THREE.MeshPhongMaterial({color: 'green'});
 
 class VoxelWorld {
     constructor(cellSize){
@@ -64,7 +51,7 @@ class VoxelWorld {
             return 0;
         }
         const {cellSize} = this;
-        const voxelOffset = voxelY * cellSize * cellSize + voxelZ * cellSize + voxelX;
+        const voxelOffset = this.computeVoxelOffset(x, y, z);
         return cell[voxelOffset];
     }
     generateGeometryDataForCell(cellX, cellY, cellZ){
@@ -172,21 +159,66 @@ VoxelWorld.faces = [
     }
 ];
 
-// you could put all the code above into its own js file. 
-// TODO: Research es_modules
 
 const cellSize = 32;
+
+const fov = 75;
+const aspect = 2; // the canvas default
+const near = 0.1;
+const far = 1000;
+const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+camera.position.set( - cellSize * .3, cellSize * .8, - cellSize * .3 );
+
+const controls = new OrbitControls( camera, canvas );
+controls.target.set( cellSize / 2, cellSize / 3, cellSize / 2 );
+controls.update();
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 'lightblue' );
+
+function addLight( x, y, z ) {
+
+    const color = 0xFFFFFF;
+    const intensity = 3;
+    const light = new THREE.DirectionalLight( color, intensity );
+    light.position.set( x, y, z );
+    scene.add( light );
+
+}
+
+addLight( - 1, 2, 4 );
+addLight( 1, - 1, - 2 );
 
 const world = new VoxelWorld(cellSize);
 
 for (let y = 0; y < cellSize; ++y){
     for (let z = 0; z < cellSize; ++z) {
         for (let x = 0; x < cellSize; ++x) {
-            const height = ()
+            const height = (Math.sin(x / cellSize * Math.PI * 2) + Math.sin(z/cellSize * Math.PI * 3)) * (cellSize / 6) + (cellSize / 2);
+            if (y < height) {
+                world.setVoxel*(x, y, z, 1);
+            }
         }
 
     }
 }
+
+const {positions, normals, indices} = world.generateGeometryDataForCell(0,0,0);
+const geometry = new THREE.BufferGeometry();
+const material = new THREE.MeshLambertMaterial({color: 'red'});
+const positionNumComponents = 3;
+const normalNumComponents = 3;
+geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(positions), positionNumComponents));
+geometry.setAttribute('normal', new THREE.BufferAttribute(new Float32Array(normals), normalNumComponents));
+geometry.setIndex(indices);
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(TARGET_WIDTH, TARGET_HEIGHT);
+renderer.setClearColor(0xb8fcda, 1);
+document.body.appendChild(renderer.domElement);
 
 
 
