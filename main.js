bc412272b7d8119bbf2e8b05096ab7baa8c7be6f
ryoma <ryomams@users.noreply.github.com>
2023-09-06 23:30:38 -0700

    finished cube generation

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ console.log("good morning!")
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+//basic camera and canvas render bullshit im gonna SCREAM
 const TARGET_WIDTH = 1280;
 const TARGET_HEIGHT = 720;
 
@@ -23,17 +24,36 @@ const camera = new THREE.OrthographicCamera(TARGET_WIDTH / -2, TARGET_WIDTH / 2,
 const box_geometry_fornow_ = new THREE.BoxGeometry(1, 1, 1);
 const box_material_fornow_ = new THREE.MeshPhongMaterial({color: 'green'});
 
-class env_cube {
-	constructor(in_size, in_pos, in_type){
-		this.Size = in_size;
-		this.Geometry = new THREE.BoxGeometry(this.Size.x, this.Size.y, this.Size.z);
-		
-		
-	}
-	render() {
-
+const cubes = new Uint8Array(512); // 8^3    // this should be 512B in size.
+function generateWorld() {
+	for(let x = 0; x < 8; ++ x){
+		for(let y = 0; y < 8; ++ y){
+			for(let z = 0; z < 8; ++ z){
+				const blockid = x * 8 * 8 + y * 8 + z;
+				cubes[blockid] = 1;
+			}
+		}
 	}
 }
+generateWorld();
+function renderCube(in_x, in_y, in_z) {
+	const mesh = new THREE.Mesh(box_geometry_fornow_, box_material_fornow_);
+	mesh.position.set(in_x, in_y , in_z);
+	scene.add(mesh);
+	console.log("gay sex!!");
+}
+function renderWorld() {
+	for(let x = 0; x < 8; ++ x){
+		for(let y = 0; y < 8; ++ y){
+			for(let z = 0; z < 8; ++ z){
+				renderCube(x, y, z);
+			}
+		}
+	}
+}
+renderWorld();
+
+
 
 
 function animate() {
@@ -42,3 +62,20 @@ function animate() {
 }
 
 animate();
+
+
+
+
+
+function addLight( x, y, z ) {
+
+	const color = 0xFFFFFF;
+	const intensity = 3;
+	const light = new THREE.DirectionalLight( color, intensity );
+	light.position.set( x, y, z );
+	scene.add( light );
+
+}
+
+addLight( - 1, 2, 4 );
+addLight( 1, - 1, - 2 );
