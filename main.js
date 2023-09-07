c5b1e1b6e1cd5f991ef37e9ef7b8dea2fc8ca0ea
ryoma <ryomams@users.noreply.github.com>
2023-09-07 01:28:04 -0700

    randomized blocks

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ console.log("good morning!")
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max);
+  }
+
 //basic camera and canvas render bullshit im gonna SCREAM
 const TARGET_WIDTH = 1280;
 const TARGET_HEIGHT = 720;
@@ -29,7 +33,9 @@ controls.target.set( 8 / 2, 8 / 3, 8 / 2 );
 controls.update();
 
 const box_geometry_fornow_ = new THREE.BoxGeometry(1, 1, 1);
-const box_material_fornow_ = new THREE.MeshPhongMaterial({color: 'green'});
+const box_material_fornow_1 = new THREE.MeshPhongMaterial({color: 'green'});
+const box_material_fornow_2 = new THREE.MeshPhongMaterial({color: 'red'});
+const box_material_fornow_3 = new THREE.MeshPhongMaterial({color: 'blue'});
 
 const cubes = new Uint8Array(512); // 8^3    // this should be 512B in size.
 function generateWorld() {
@@ -37,28 +43,57 @@ function generateWorld() {
 		for(let y = 0; y < 8; ++ y){
 			for(let z = 0; z < 8; ++ z){
 				const blockid = x * 8 * 8 + y * 8 + z;
-				cubes[blockid] = 1;
+				switch (getRandomInt(3)) {
+					case (0):
+						cubes[blockid] = 1;
+						break;
+					case (1):
+						cubes[blockid] = 2;
+						break;
+					case (2):
+						cubes[blockid] = 3;
+						break;
+				}
 			}
 		}
 	}
 }
+
 generateWorld();
-function renderCube(in_x, in_y, in_z) {
-	const mesh = new THREE.Mesh(box_geometry_fornow_, box_material_fornow_);
+function renderCube(in_x, in_y, in_z, in_material) {
+	const mesh = new THREE.Mesh(box_geometry_fornow_, in_material);
 	mesh.position.set(in_x, in_y , in_z);
 	scene.add(mesh);
 	console.log("gay sex!!");
 }
+
 function renderWorld() {
 	for(let x = 0; x < 8; ++ x){
 		for(let y = 0; y < 8; ++ y){
 			for(let z = 0; z < 8; ++ z){
-				renderCube(x, y, z);
+				const blockid = x * 8 * 8 + y * 8 + z;
+				switch (cubes[blockid]) {
+					case (1):
+						renderCube(x, y, z, box_material_fornow_1);
+						break;
+					case (2):
+						renderCube(x, y, z, box_material_fornow_2);
+						break;
+					case (3):
+						renderCube(x, y, z, box_material_fornow_3);
+						break;
+				}
 			}
 		}
 	}
 }
 renderWorld();
+function selectCube(id){
+	
+}
+function setCube(id, value){
+
+}
 
 
 
